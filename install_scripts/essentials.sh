3a689dd4380e7aaca40077f60730eb4165e9a77c
Mads Byriel <madshbyriel@gmail.com>
2026-07-06 17:04:40 +0200

    fix: initial commit

---
+++ install_scripts/essentials.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --noconfirm --needed \
+    base-devel \
+    tldr \
+    git \
+    go \
+    cmake \
+    ninja \
+    neovim \
+    nodejs \
+    npm \
+    tmux \
+    wget
